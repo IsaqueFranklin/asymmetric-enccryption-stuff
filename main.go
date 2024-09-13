d9a9eca4d4de220d35c09da04954cd2c15ba96bc
Isaque Franklin <isaquefrankli@gmail.com>
2024-09-12 23:08:05 -0300

    More stuff.

--- main.go
+++ main.go
@@ -17,5 +17,25 @@ func main() {
   publicKey := &privateKey.PublicKey
 
   prrivateKeyBytes := x509.MarshalPKCS1PrivateKey(prrivateKey)
-  privateKeyPEM := 
+  privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+    Type: "RSA PRIVATE KEY",
+    Bytes: prrivateKeyBytes,
+  }) 
+  err = os.WriteFile("private.pem", privateKeyPEM, 0644)
+  if err != nil {
+    panic(err)
+  }
+
+  publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+    if err != nil {
+        panic(err)
+    }
+    publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+        Type:  "RSA PUBLIC KEY",
+        Bytes: publicKeyBytes,
+    })
+    err = os.WriteFile("public.pem", publicKeyPEM, 0644)
+    if err != nil {
+        panic(err)
+  }
 }
