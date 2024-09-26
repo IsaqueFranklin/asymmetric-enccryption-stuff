9e20c1b6b6cfc130d4b8f9ff2f72691f1afb8937
Isaque Franklin <isaquefrankli@gmail.com>
2024-09-26 08:11:55 -0300

    Asymmetric stuff finishing encrypting.

--- main.go
+++ main.go
@@ -37,5 +37,19 @@ func main() {
     err = os.WriteFile("public.pem", publicKeyPEM, 0644)
     if err != nil {
         panic(err)
-  }
+    }
+
+    publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+    if err != nil {
+      panic(err)
+    }
+
+    publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+      Type: "RSA PUBLIC KEY",
+      Bytes: publicKeyBytes
+    })
+    err = os.WriteFile("public.pem", publicKeyPEM, 0644)
+    if err != nil {
+      panic(err)
+    }
 }
